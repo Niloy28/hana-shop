f994421fe49bb958e260f643a9f24f7d0d43ac59
Farhan Ishrak Islam <niloy28@live.com>
2024-10-17 13:53:12 +0900

    Fix product load skeleton

--- src/app/(storefront)/products/loading.tsx
+++ src/app/(storefront)/products/loading.tsx
@@ -1,9 +1,9 @@
 import LoadingProductCard from "@/components/products/LoadingProductCard";
 import { randomUUID } from "crypto";
 
-const ProductLoadingPage = () => {
+const ProductLoadingPage = async () => {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3">
+    <div className="mx-2 mt-10 grid gap-4 p-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       {[...Array(10)].map((_) => (
         <LoadingProductCard key={randomUUID()} />
       ))}
--- src/components/products/LoadingProductCard.tsx
+++ src/components/products/LoadingProductCard.tsx
@@ -1,12 +1,21 @@
+import { Card, CardContent } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 
 const LoadingProductCard = () => {
   return (
-    // TODO: Needs UI adjustment to match actual product card.
-    <div className="flex flex-col items-center justify-center">
-      <Skeleton className="h-[600px] w-[600px]" />
-      <Skeleton className="h-full w-3" />
-    </div>
+    <Card>
+      <CardContent className="p-0 pb-2">
+        <div className="flex h-[500px] flex-col items-center justify-center gap-y-2">
+          <Skeleton className="h-2/3 w-full rounded-lg object-cover" />
+          <div className="flex h-1/3 w-full flex-col items-center justify-center gap-2 p-2">
+            <Skeleton className="h-1/4 w-full" />
+            <Skeleton className="h-1/4 w-full" />
+            <Skeleton className="h-1/4 w-full" />
+            <Skeleton className="h-1/4 w-full" />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
