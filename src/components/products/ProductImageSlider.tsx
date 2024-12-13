93da201b0f88a311b00ede4f0537c9c78434668a
Farhan Ishrak Islam <niloy28@live.com>
2024-12-14 02:30:19 +0900

    Fix product page image sizing

--- src/components/products/ProductImageSlider.tsx
+++ src/components/products/ProductImageSlider.tsx
@@ -19,61 +19,53 @@ const ProductImageSlider = ({
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   return (
-    <div className="grid items-start gap-6 md:gap-3">
-      <div className="flex max-w-screen-md flex-1 flex-col gap-2 overflow-hidden rounded-lg">
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          className="h-3/5 rounded-lg"
-        >
-          <CarouselContent>
-            {images.map((image, index) => (
-              <CarouselItem key={index}>
-                <Image
-                  src={image}
-                  width={600}
-                  height={600}
-                  alt="Product image"
-                  className="w-full rounded-lg object-cover"
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious className="ml-16" />
-          <CarouselNext className="mr-16" />
-        </Carousel>
+    <div className="overflow-hidden rounded-lg">
+      <Carousel className="w-full">
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem key={index} className="flex">
+              <Image
+                src={image}
+                width={600}
+                height={400}
+                alt="Product image"
+                className="w-full rounded-lg object-cover"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="ml-16" />
+        <CarouselNext className="mr-16" />
+      </Carousel>
 
-        <Carousel opts={{ loop: true }} className="rounded-lg">
-          <CarouselContent>
-            {images.map((image, index) => (
-              <CarouselItem
-                key={index}
-                className="basis-1/4 hover:cursor-pointer"
-                onClick={() => {
-                  setCurrentImageIndex(index);
-                }}
-              >
-                <Image
-                  src={image}
-                  width={600}
-                  height={600}
-                  alt="Product image"
-                  className={cn(
-                    "h-full w-full rounded-lg object-cover",
-                    currentImageIndex === index
-                      ? "rounded-lg border-2 border-secondary-foreground"
-                      : "",
-                  )}
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
+      <Carousel className="mt-4 rounded-lg">
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem
+              key={index}
+              className="basis-1/4 hover:cursor-pointer"
+              onClick={() => {
+                setCurrentImageIndex(index);
+              }}
+            >
+              <Image
+                src={image}
+                width={600}
+                height={400}
+                alt="Product image"
+                className={cn(
+                  "h-full w-full rounded-lg object-cover",
+                  currentImageIndex === index
+                    ? "rounded-lg border-2 border-secondary-foreground"
+                    : "",
+                )}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
