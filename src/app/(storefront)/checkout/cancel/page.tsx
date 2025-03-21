b5f6b3066a3f986c953d2e67f47a7b0235ad2bc6
Farhan Ishrak Islam <niloy28@live.com>
2025-03-21 19:56:46 +0900

    Fix checkout pages theming

--- src/app/(storefront)/checkout/cancel/page.tsx
+++ src/app/(storefront)/checkout/cancel/page.tsx
@@ -4,17 +4,15 @@ import Link from "next/link";
 
 const CheckoutCancelPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
-        <h1 className="text-center text-3xl font-bold text-red-500">
-          Order Cancelled
-        </h1>
-        <p className="mt-4 text-center text-gray-700">
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full max-w-md rounded-lg p-8 shadow-lg">
+        <h1 className="text-center text-3xl font-bold">Order Cancelled</h1>
+        <p className="mt-4 text-center">
           Your order has been cancelled. If you have any questions, please
           contact our support team.
         </p>
         <div className="mt-6 flex flex-col justify-center gap-2">
-          <Button className="rounded-md bg-red-500 px-4 py-2 text-white">
+          <Button className="rounded-md bg-red-500 px-4 py-2 hover:bg-red-700">
             <Link href="/products">
               <div className="flex justify-center gap-2">
                 <ShoppingBagIcon />
@@ -22,7 +20,7 @@ const CheckoutCancelPage = () => {
               </div>
             </Link>
           </Button>
-          <Button className="rounded-md bg-blue-500 px-4 py-2 text-white">
+          <Button className="rounded-md bg-primary px-4 py-2">
             <Link href="/cart">
               <div className="flex justify-center gap-2">
                 <ShoppingCartIcon />
--- src/app/(storefront)/checkout/success/page.tsx
+++ src/app/(storefront)/checkout/success/page.tsx
@@ -4,16 +4,16 @@ import Link from "next/link";
 
 const CheckoutSuccessPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full max-w-md rounded-lg p-8 shadow-lg">
         <h1 className="text-center text-3xl font-bold text-green-500">
           Success!
         </h1>
-        <p className="mt-4 text-center text-gray-700">
+        <p className="mt-4 text-center">
           Your order has been placed successfully.
         </p>
         <div className="mt-6 flex justify-center">
-          <Button className="rounded-md bg-green-500 px-4 py-2 text-white">
+          <Button className="rounded-md bg-green-500 px-4 py-2 hover:bg-green-700">
             <Link href="/products">
               <div className="flex justify-center gap-2">
                 <ShoppingBagIcon />
